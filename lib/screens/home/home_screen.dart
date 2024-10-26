4d17ad0e7c5357b8cbd417f10e2ed2aa2a6c5bb9
Jinali Chavda <chavdajinali@gmail.com>
2024-10-26 16:23:54 +0530

    change signup fields

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -39,7 +39,7 @@ class _HomeScreenState extends State<HomeScreen> {
         return Scaffold(
           appBar: AppBar(
             title: const Text(
-              'Clinic Dashboard',
+              'Vatsalya Speech & Hearing Clinic Dashboard',
               style: TextStyle(color: Colors.white),
             ),
             backgroundColor: Colors.blue,
